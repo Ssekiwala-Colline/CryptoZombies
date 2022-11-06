0364c6bad65b6449a30e5e59fc338d335f25b33b
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-11-06 10:09:26 +0300

    #1 Zombie description

--- contract.sol
+++ contract.sol
@@ -2,7 +2,23 @@
 pragma solidity >0.8.0;
 
 contract ZombieFactory {
+    
+    //The zombie will be determined by a 16 digit number
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
+    //store the features of the zombie
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+    
+    //Let's now create the zombies using a function
+    function createZombie(string memory _name, uint _dna) public {
+        
+    } 
+
 }
+
