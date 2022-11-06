1e16bf64683c9f16f41499769cca2635dc06d969
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-11-06 13:24:23 +0300

    #3 Add Events

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -2,6 +2,9 @@
 pragma solidity >0.8.0;
 
 contract ZombieFactory {
+
+    // let our front-end know every time a new zombie was created, so the app can display it.
+    event NewZombie(uint zombieID, string name, uint dna);
     
     //The zombie will be determined by a 16 digit number
     uint dnaDigits = 16;
@@ -18,7 +21,6 @@ contract ZombieFactory {
     //Adding a zombie to the factory based on the Name and DNA
     //Let's restrict external contracts from creating a zombie
     function _createZombie(string memory _name, uint _dna) private {
-
         zombies.push(Zombie(_name, _dna));
     } 
 
